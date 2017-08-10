85e9009ac6eaaa60dd3a1994d2e48f9249c0d0f4
ch_sss <sdhhqb@gmail.com>
2017-08-10 14:26:51 +0800

    optimize config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,16 +3,17 @@ const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = function(env, argv) {
 
   let publicPath = '/';
-  let tmpFile = './src/template.html';
+  let tmpFile = './src/templates/template.html';
   if (env && env.github) {
     console.log('env : ', env);
     publicPath = '/cxxsn_com/';
-    tmpFile = './src/template_github.html';
+    tmpFile = './src/templates/template_github.html';
   }
 
   return {
@@ -52,31 +53,46 @@ module.exports = function(env, argv) {
     },
 
     plugins: [
-      new CleanWebpackPlugin(['docs'], {exclude: ['image', 'lib', '404.html', 'favicon.ico']}),
+      // 清空之前打包的文件
+      new CleanWebpackPlugin(['docs'], {exclude: ['image', 'lib']}),
 
+      // 设置为生产环境
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify('production')
       }),
 
+      // 提取单独vendor分块
       new webpack.optimize.CommonsChunkPlugin({
         name: 'vendor',
         minChunks: Infinity
       }),
 
+      // 配合vendor，避免每次打包vendor的hash改变
       new webpack.optimize.CommonsChunkPlugin({
         name: 'runtime'
       }),
 
+      // 配合vendor，避免每次打包vendor的hash改变
       new webpack.HashedModuleIdsPlugin(),
 
+      // 提取css
       new ExtractTextPlugin({filename: 'style.[contenthash].css'}),
 
+      // 压缩js
       new UglifyJSPlugin(),
 
+      // 转换html模板
       new HtmlWebpackPlugin({
         template: tmpFile,
         inject: 'body'
-      })
+      }),
+
+      // 复制文件
+      new CopyWebpackPlugin([
+        { from: 'src/static/baidu_verify_fdvrUl7wz2.html' },
+        { from: 'src/static/favicon.ico' },
+        { from: 'src/static/404.html' }
+      ])
 
     ]
   };
